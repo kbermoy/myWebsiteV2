ccaa8d1bbfbe4bf53af29e31e8ffacb61a3cbde6
kristine bermoy <kbermoy02@gmail.com>
2023-12-03 20:22:41 -0800

    completed the design of the modal

---
+++ script.js
@@ -0,0 +1,11 @@
+const section = document.querySelectorAll('section');
+const darkMode = document.querySelectorAll('darkMode')
+
+function darkModeToggle() {
+    section.classList.toggle('sectionDarkMode')
+}
+
+
+// adding event listener
+
+
