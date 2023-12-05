c6c9248b83e38a8ccbaea02a9adf4d5517ff3ae8
kristine bermoy <kbermoy02@gmail.com>
2023-12-04 19:32:04 -0800

    added the email service

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-const section = document.querySelectorAll('section');
-const darkMode = document.querySelectorAll('darkMode')
+const loading = document.querySelector('.modal__overlay--loading')
+const success = document.querySelector('.modal__overlay--success')
 
-function darkModeToggle() {
-    section.classList.toggle('sectionDarkMode')
+function contact(event) {
+    event.preventDefault();
+    loading.classList += ' modal__overlay--visible'
+    
+    emailjs
+        .sendForm (
+            'service_a91zgae',
+            'template_olmehqj',
+            event.target,
+            'D6l8IRVfDinaWvoI6'
+    ).then(() => {
+        loading.classList.remove('modal__overlay--visible')
+        success.classList += ' modal__overlay--visible'
+    }).catch(() => {
+        loading.classList.remove('modal__overlay--visible')
+        alert('The email service is temporarily unavailable. Please contact me directly on kbermoy02@gmail.com')
+    })
+    
+    
+
+    setTimeout(() => {
+        
+        console.log('worked 1')
+    }, 1000)
 }
-
-
-// adding event listener
-
-
